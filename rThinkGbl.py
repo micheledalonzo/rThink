120814ee394f1bfa72df21a5b875cfbc8de9eefa
micheledalonzo <michele.dalonzo@gmail.com>
2014-08-01 22:51:54 +0200

    0.3

--- rThinkGbl.py
+++ rThinkGbl.py
@@ -1,5 +1,4 @@
 # -*- coding: cp1252 -*-.
-from rThinkFunctions import *
 import logging
 from rThinkDb import *
 from rThinkFunctions import *
@@ -8,8 +7,13 @@ import difflib
 import pypyodbc
 import datetime
 import re
+import inspect
+import traceback
+import logging
+import sys
 
 # init var
+CountryISO      = {}
 RunDate         = SetNow()
 GmapNumcalls    = 0
 count           = 0
@@ -21,10 +25,15 @@ cSql            = None   #
 SqLite          = None   # connection
 cLite           = None   # cursore sqlite
 RunId           = 0
-
+wait            = 0
 # database flag YES/NO
 YES             = -1
 NO              = 0
+queue_fn        = "" 
+nxpage_fn       = "" 
+parse_fn        = "" 
+restart         = False
+currency        = ""
 
 preposizioni  = ["il","lo","l'","la","i","gli","le", "di","del","dello","dell'","della","dei","degli","delle","a","al",\
                  "allo","all'","alla","ai","agli","alle","da","dal","dallo","dall'","dalla","dai","dagli","dalle","in","nel",\
@@ -35,3 +44,67 @@ punteggiatura = [".", ",", ";", ":", "?", "!", "?!", "...", "\"", "'", "<", ">",
 congiunzioni =  ["&","pure", "inoltre", "ancora", "altresì", "ma", "però", "pure", "mentre", "anzi", "invece", "tuttavia", "dunque", "però",\
                  "quindi", "ondeperciò", "pertanto", "ebbene", "laondee", "pure", "né","inoltre", "ancora", "neppure", "neanche", "nemmeno", \
                  "e", "né", "o", "come", "così","sia", "che","quanto", "quale", "difatti", "cioè", "invero", "ossiaossia", "ovvero", "oppure"]
+
+INFO     = logging.INFO
+CRITICAL = logging.CRITICAL
+FATAL    = logging.FATAL
+DEBUG    = logging.DEBUG
+ERROR    = logging.ERROR
+CRITICAL = logging.CRITICAL
+WARN     = logging.WARN
+WARNING  = logging.WARNING
+
+#datefmt='%d-%m %H:%M'
+
+
+# console
+logger = logging.getLogger("mylogger")
+chformat = logging.Formatter("[%(levelname)-8s] [%(message)-70s]")
+chandler = logging.StreamHandler(stream=sys.stdout)
+chandler.setFormatter(chformat)
+chandler.setLevel(logging.INFO)
+logger.addHandler(chandler)
+logger.setLevel(logging.DEBUG)
+# file
+#fh = logging.getLogger("mylogger")
+fhformat = logging.Formatter('[%(levelname)-8s] [%(asctime)s] [%(message)s]', "%d-%m %H:%M:%S")
+fhandler = logging.FileHandler("Test.log", mode='w')
+fhandler.setFormatter(fhformat)
+fhandler.setLevel(logging.DEBUG)
+logger.addHandler(fhandler)
+logger.setLevel(logging.DEBUG)
+
+#logger.debug("This is a debug message.")
+#logger.info("Some info message.")
+#logger.warning("A warning.")
+#logger.warning(now)
+
+
+def log(level, message=''):
+
+    if level == DEBUG:
+        #frame = inspect.currentframe()
+        #stack_trace = traceback.format_stack(frame)
+        runmsg = "--> %s" % (inspect.stack()[1][3])   # nome della funzione
+        logger.debug(runmsg)
+        if len(message) > 0:
+            logger.debug(message)    
+        #logging.debug(stack_trace[:-1])
+
+    if level == INFO:
+        logger.info(message)
+
+    if level == WARNING or level == WARN:
+        logger.warn(message)
+    
+    if level == ERROR:
+        #frame = inspect.currentframe()
+        #stack_trace = traceback.format_stack(frame)
+        logger.error(message)
+        #logging.error(stack_trace[:-1])
+        
+    if level == CRITICAL or level == FATAL:
+        frame = inspect.currentframe()
+        stack_trace = traceback.format_stack(frame)
+        logger.critical(message)
+        logger.critical(stack_trace[:-1])
